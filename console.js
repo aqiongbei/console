6a4844e3a15127d08aea37136f40743e792fa2e6
aqiongbei <aqiongbei@gamil.com>
2017-12-05 20:54:20 +0800

    init my console

---
+++ console.js
@@ -0,0 +1,47 @@
+// var console = (function (){
+// 	var _console = console;
+// 	function log() {
+
+// 	}
+// 	function warn() {
+
+// 	}
+// 	return {
+// 		log();
+// 	};
+// })();
+
+// $.console = console;
+// 
+// 
+// 
+function _console (con){
+	var str = 'aqiongbei@console /$: ' + (con || '%cplease type your word');
+	var lineLen = 500;
+	var index = 0;
+	var len = str.length;
+	var info = 'color: rgba(0, 255, 0, 1); background: rgba( 0, 0, 0, 1); font-size: 1.2em; line-height: 20px';
+	var warn = 'color: rgba(255, 0, 0, 1); background: rgba( 0, 0, 0, 1); font-size: 1.2em; line-height: 20px';
+	var timer = setInterval(function (){
+		if (index > len) {
+			clearInterval(timer);
+			// console.log('%c' + formatStr + 'GAME OVER' + formatStr, css);
+		} else {
+			console.clear();
+			if (con) {
+				console.log('%c' + str.slice(0, index) + formatLine(lineLen - index - 2), info)
+			} else {
+				console.log('%c' + str.slice(0, index) + formatLine(lineLen - index - 2), info, warn)
+			}
+			index ++;
+		}
+	}, 104)
+}
+
+function formatLine(len){
+	var str = ' ';
+	for (var i = 0; i <= len; i ++) {
+		str += ' ';
+	}
+	return str;
+}
